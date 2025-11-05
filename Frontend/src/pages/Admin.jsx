9d07e87acad84179777b0138bedfcb33781153d1
VarakornSPU <varakon.mat@spumail.net>
2025-11-05 17:07:56 +0700

    แก้ Error หน้า Admin

--- Frontend/src/pages/Admin.jsx
+++ Frontend/src/pages/Admin.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import AddProductForm from "../components/AddProductForm";
 import "../style/Admin.css";
